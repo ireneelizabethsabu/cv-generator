73233bb1f696cbbf015294b351b51b4be7f6b1ac
Lza-etc <68897171+Lza-etc@users.noreply.github.com>
2021-09-05 02:39:36 +0530

    added utils

--- src/api/index.js
+++ src/api/index.js
@@ -1,37 +1,57 @@
-import axios from 'axios';
+import axios from "axios";
 
 const getUsers = async (nam) => {
-    try{
-        return await axios.get("https://api.github.com/users/"+ nam);
-    }
-     catch(err){
-      console.log("User API call not complete");  
-    }
+  try {
+    return await axios.get("https://api.github.com/users/" + nam);
+  } catch (err) {
+    console.log("User API call not complete");
+  }
 };
 
-let repo=[];
+const getOrgs = async (nam) => {
+  let organization = [];
+  try {
+    const orgs = await axios.get(
+      "https://api.github.com/users/" + nam + "/orgs"
+    );
 
+    orgs.data.map(async (ele) => {
+      const org = await axios.get(ele.url);
+
+      organization.push({
+        name: org.data.name,
+        image: ele.avatar_url,
+        description: ele.description,
+        blog: org.data.blog,
+      });
+    });
+    return organization;
+  } catch (err) {
+    console.log("User API call not complete");
+  }
+};
 
 const getRepos = async (nam) => {
-    try{
-        const repos= await axios.get("https://api.github.com/users/"+nam+"/repos");
-        repos.data.map(async (ele)=>{
+  let repo = [];
+  try {
+    const repos = await axios.get(
+      "https://api.github.com/users/" + nam + "/repos"
+    );
+    repos.data.map(async (ele) => {
+      if (!ele.fork) {
+        const lang = await axios.get(ele.languages_url);
+        repo.push({
+          name: ele.full_name,
+          lang: lang.data,
+          stargazer_count: ele.stargazers_count,
+        });
+      }
+    });
 
-            const lang = await axios.get(ele.languages_url);
-            // console.log(lang);
-            repo.push({
-            name:ele.full_name,
-            lang:lang.data,
-            stargazer_count:ele.stargazers_count
-            });
-        })
-        // console.log(repo);
-        return repo;
-    }
-    catch(err){
-      console.log("Repos API call not complete");  
-    }
-  
+    return repo;
+  } catch (err) {
+    console.log("Repos API call not complete");
+  }
 };
 
-export  {getUsers, getRepos};
+export { getUsers, getRepos, getOrgs };
---
+++ src/api/utils.js
@@ -0,0 +1,34 @@
+import { getRepos } from "./index";
+
+const langPercent = (username) => {
+  let languages = [];
+  let totalMB = 0;
+  let no = 0;
+  getRepos(username).then((repos) => {
+    {
+      repos.map((repo) => {
+        if (Object.entries(repo.lang).length !== 0) {
+          Object.keys(repo.lang).map((ele) => {
+            if (!(ele in languages)) {
+              languages[ele] = repo.lang[ele] / 1024;
+
+              totalMB += repo.lang[ele] / 1024;
+            } else {
+              languages[ele] += repo.lang[ele] / 1024;
+              totalMB += repo.lang[ele] / 1024;
+            }
+          });
+        }
+      });
+
+      Object.keys(languages).map((lang) => {
+        languages[lang] *= 100 / totalMB;
+        no += languages[lang];
+      });
+      console.log(no);
+    }
+  });
+  console.log(languages);
+};
+
+export { langPercent };
