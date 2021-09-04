ccbcc6adb26b6f36290cc6f72ba737ef79f7c3b6
unknown <aishwaryaaj2002@gmail.com>
2021-09-04 14:33:42 +0530

    added Initial home page template

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,34 @@
 import React from "react";
-import Header from "./Header/header";
+import {Button, Form,Row,Col, Container} from 'react-bootstrap';
+import './Home.css';
 
 export const Home = () => {
     return(
-        <div>
-            <Header/>
-        </div>
+        <Container fluid className="min-vh-100 ">
+            <Row xs={1} sm={1} md={2} >
+                <Col className="text-center">
+                <h1 className="title">Get your customized cv with <br /><span id="head">CV-GENERATOR</span></h1>
+                <p className="lead">
+                    Generates CV Based on Github Profile 
+                </p>
+                </Col>
+
+                <Col md={3} className="d-flex justify-content-center">
+                <Form>
+                    <Form.Group>
+                        <Form.Label className="text-center"> Enter Github user id</Form.Label>
+                        <Form.Control  type="username" placeholder="user-id" />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Check type="checkbox" label="Include personal details" />
+                    </Form.Group>
+                    <Button variant="outline-dark" type="submit" >
+                        Generate CV
+                    </Button>
+                </Form>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
